d88253ee50b7004aa697523c1b6a98118c3189cc
Shakil148 <shakilpp148@gmail.com>
2020-10-27 17:53:13 +0600

    user list added

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,27 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    List of all User
+                    <table class="table">
+                        <thead>
+                            <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Phone</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($user as $users)
+                            <tr>
+                            <th scope="row">{{$loop->iteration}}</th>
+                            <td>{{$users->name}}</td>
+                            <td>{{$users->email}}</td>
+                            <td>{{$users->phone}}</td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
